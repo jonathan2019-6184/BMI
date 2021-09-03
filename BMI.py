c737ebe5166a5b57464af706d5107fd768968743
Jonathan Tinoy <80259364+jonathan2019-6184@users.noreply.github.com>
2021-09-03 16:56:45 +0800

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -5,42 +5,38 @@
 #Implement a Python program that prompts a user for their height and weight. Height  should be entered as inches and weight should be entered in pounds. Perform the  calculation in units of kilograms and meters as shown in the chart. Compare the result to  the information in the chart. Use class(es) in your program design. 
 
 
-class CalBMI:
-    def _init_(get_height, get_weight, body_mass_index):
-    bmi()
-    
+class BMI:
 
-#define variables
-get_height = 0.0
-get_weight = 0.0
-body_mass_index = 0.0
-
-
-#We will start with the introduction to our BMI (Body Mass Index)
-def bmi():
     print("Welcome to my BMI calculator!")
     print("If you can tell me your weight and height")
     print("I can tell you your Body Mass Index")
     print("Thank You!\n")
 
-#From this point I will ask the user for his/her information.
-
-get_height = float(input("Please enter your height in inches. "))
-get_weight = float(input("Please enter your weight in pounds. "))
-
-#user will enter there information above and we will then calcualte.
-body_mass_index = get_weight / (get_height/100)**2
+    Inches = float(input("Please enter your height in inches: "))
+    Pounds = float(input("Please enter your weight in pounds: "))
+    get_height = Inches * 0.0254
+    get_weight = Pounds / 2.2
 
 
+    def BMI(get_height, get_weight):
+        body_mass_index = round(get_weight/ (get_height ** 2), 1) 
 
+        return body_mass_index
 
-if body_mass_index < 18.5:
-    print("A person with a BMI of " + str(body_mass_index ) + " is underwieght ")
-elif body_mass_index < 24.9:
-    print("A person with a BMI of " + str(body_mass_index ) + " is normal weight ")
-elif body_mass_index < 30:
-    print("A person with a BMI of " + str(body_mass_index ) + " is overweight ")
-elif body_mass_index >30:
-    print("A person with a BMI of" + str(body_mass_index) + "is obese" )
+    body_mass_index = BMI(get_height, get_weight)
+    print("\nYour BMI is", format(body_mass_index), end='')
 
-main()
+    if body_mass_index <= 18.5:
+        print(', it means you are underweight.')
+
+    elif body_mass_index > 18.5 and body_mass_index < 25:
+        print(', it means you are normal.')
+
+    elif body_mass_index >25 and body_mass_index <30:
+        print(', it means you are overweight.')
+
+    elif body_mass_index > 30:
+        print(', it means you are obese.')
+
+    else:
+        print('THERE IS AN ERROR OF YOUR INPUT.')
